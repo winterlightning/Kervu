1efe969ccdfdffe03d18600eb21c1b601c59b865
R <ray@simplrmanage.com>
2010-04-06 01:05:28 +0000

    added craigslist features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < BaseController
   end
 
   # Filters
-  before_filter :login_required, :only => [:edit, :edit_account, :update, :welcome_photo, :welcome_about, 
+  before_filter :login_required, :only => [:edit, :edit_account, :update, :welcome_photo, :welcome_about, :embedding,
                                           :welcome_invite, :return_admin, :assume, :featured,
                                           :toggle_featured, :edit_pro_details, :update_pro_details, :dashboard, :deactivate, 
                                           :crop_profile_photo, :upload_profile_photo]
@@ -97,23 +97,9 @@ class UsersController < BaseController
     render :layout => false
   end
   
-  def craigslistView  
-    @friend_count               = @user.accepted_friendships.count
-    @accepted_friendships       = @user.accepted_friendships.find(:all, :limit => 5).collect{|f| f.friend }
-    @pending_friendships_count  = @user.pending_friendships.count()
-
-    @comments       = @user.comments.find(:all, :limit => 10, :order => 'created_at DESC')
-    @photo_comments = Comment.find_photo_comments_for(@user)    
-    @users_comments = Comment.find_comments_by_user(@user, :limit => 5)
-
-    @recent_posts   = @user.posts.find(:all, :limit => 2, :order => "published_at DESC")
-    @clippings      = @user.clippings.find(:all, :limit => 5)
-    @photos         = @user.photos.find(:all, :limit => 5)
-    @comment        = Comment.new(params[:comment])
-    
-    @my_activity = Activity.recent.by_users([@user.id]).find(:all, :limit => 10) 
-
-    update_view_count(@user) unless current_user && current_user.eql?(@user)
+  def embedding  
+    @user = current_user
+    @photos = @user.photos.find(:all, :limit => 5)
   end
   
   def new
